ce40d289fe26f6ca8890e9dda5de678524bb0fbb
dmu <dmu@dbh.de>
2022-09-23 14:20:25 +0200

    Mapping-Pfad

--- src/main/java/org/dmu/gameservice/controller/GameController.java
+++ src/main/java/org/dmu/gameservice/controller/GameController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Hier wird die Rest-API definiert.
  */
 @RestController
+@RequestMapping("players")
 @CrossOrigin(origins = { "http://localhost:3000"})
 @Data
 @RequiredArgsConstructor
@@ -24,27 +26,27 @@ public class GameController {
 
     final private GameService service;
 
-    @PutMapping("/players/{type}")
+    @PutMapping("/{type}")
     public Player createPlayer(@PathVariable String type) {
         return service.createPlayer(type);
     }
 
-    @GetMapping("/players/{id}")
+    @GetMapping("/{id}")
     public Player getPlayer(@PathVariable int id) {
         return service.getPlayer(id);
     }
 
-    @GetMapping("/players/winner/{botId}/{playerId}")
+    @GetMapping("/winner/{botId}/{playerId}")
     public Player getPlayerWinner(@PathVariable int botId, @PathVariable int playerId) {
         return service.getPlayerWinner(botId, playerId);
     }
 
-    @PatchMapping("/players/{id}/name/{name}")
+    @PatchMapping("/{id}/name/{name}")
     public Player updatePlayerName(@PathVariable int id, @PathVariable String name) {
     	return service.updatePlayerName(id, name);
     }
 
-    @PatchMapping("/players/{id}/symbol/{symbol}")
+    @PatchMapping("/{id}/symbol/{symbol}")
     public Player updatePlayerSymbol(@PathVariable int id, @PathVariable String symbol) {
     	return service.updatePlayerSymbol(id, symbol);
     }
--- src/main/java/org/dmu/gameservice/controller/SettingsController.java
+++ src/main/java/org/dmu/gameservice/controller/SettingsController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.dmu.gameservice.service.SettingsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -14,6 +15,7 @@ import java.util.Collection;
  * Hier wird die Rest-API definiert.
  */
 @RestController
+@RequestMapping("settings")
 @CrossOrigin(origins = { "http://localhost:3000"})
 @Data
 @RequiredArgsConstructor
@@ -22,7 +24,7 @@ public class SettingsController {
 
     final private SettingsService service;
 
-    @GetMapping("/settings/symbols")
+    @GetMapping("/symbols")
     public Collection<String> getSymbols() {
         return service.getSymbols();
     }
